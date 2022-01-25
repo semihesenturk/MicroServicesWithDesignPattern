8a2c72bb2957c989fd8dc8b00856ae2d189076e8
semihesenturkdev <semih.esenturk@dygdigital.com>
2022-01-25 16:05:04 +0300

    Stock Microservice has created

--- Stock.API/Program.cs
+++ Stock.API/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using Stock.API.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<AppDbContext>(options =>
+        {
+            options.UseInMemoryDatabase("StokDb");
+        }
+    );
 
 var app = builder.Build();
 
@@ -21,5 +29,16 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+#region SeedStockData
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+    context.Stocks.Add(new Stock.API.Models.Stock() { Id = 1, ProductId = 1, Count = 100 });
+    context.Stocks.Add(new Stock.API.Models.Stock() { Id = 2, ProductId = 2, Count = 100 });
+    context.SaveChanges();
+} 
+#endregion
 
 app.Run();
